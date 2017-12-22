255f4d87470c666d7cc9b336e1abde02f2bf13a4
HadrienMP <public@hadrienmp.fr>
2017-12-22 23:50:31 +0200

    Refactor, test fix and i18n preparation

--- src/main/kotlin/fr/hadrienmp/random_estimates/domain/Bot.kt
+++ src/main/kotlin/fr/hadrienmp/random_estimates/domain/Bot.kt
@@ -1,10 +1,23 @@
 package fr.hadrienmp.random_estimates.domain
 
-class Bot {
-    private val estimates = Estimates()
-    private val interpret = Interpret()
+import fr.hadrienmp.random_estimates.lib.Cache
+import fr.hadrienmp.random_estimates.lib.ClassPathFileLines
+import fr.hadrienmp.random_estimates.lib.Random
+import java.time.Duration
+import java.util.*
 
-    fun response(): String = interpret.phrase(estimates.random())
+class Bot {
+    val estimates = Cache(ClassPathFileLines("data/estimates.txt"), Duration.ofMinutes(5))
+    val units = Cache(ClassPathFileLines("data/units.txt"), Duration.ofMinutes(5))
+    val phrases = Cache(ClassPathFileLines("data/phrases.txt"), Duration.ofMinutes(5))
+
+    fun response(locale: Locale = Locale.FRANCE): String {
+        val estimate = estimate()
+        return phrase(estimate)
+    }
+
+    private fun phrase(estimate: Estimate) = Random(phrases).value().format(estimate)
+    private fun estimate() = Estimate(Random(estimates), Random(units))
 
 }
 
